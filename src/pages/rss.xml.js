c24490b7fd5b3ba05c79ff3b961dee1d5f3a4153
ptbrown99 <ptbrown99@hotmail.com>
2023-06-09 14:55:14 +0100

    update rss site url

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -4,8 +4,10 @@ export async function get() {
   return rss({
     title: 'Astro Learner | Blog',
     description: 'My journey learning Astro',
-    site: 'https://spectacular-eclair-6c2a59.netlify.app/blog/',
+    site: 'https://spectacular-eclair-6c2a59.netlify.app/posts/',
     items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
     customData: `<language>en-us</language>`,
   });
 }
+
+
